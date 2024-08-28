65563dbe21a3d3d9b2ef3cc205d321c454f2d19c
Vlad <vplavosin@gmail.com>
2024-08-28 21:21:58 +0300

    DBMS Homework

---
+++ src/DBMS/DBMSApp/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿using System.Windows.Forms;
+
+namespace DBMSApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelGenre = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxGenre = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelGenre
+            // 
+            this.labelGenre.AutoSize = true;
+            this.labelGenre.Location = new System.Drawing.Point(376, 5);
+            this.labelGenre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelGenre.Name = "labelGenre";
+            this.labelGenre.Size = new System.Drawing.Size(36, 13);
+            this.labelGenre.TabIndex = 14;
+            this.labelGenre.Text = "Genre";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonConnect.Location = new System.Drawing.Point(326, 411);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(112, 40);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect to DB";
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.AllowUserToAddRows = false;
+            this.dataGridViewParent.AllowUserToDeleteRows = false;
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(21, 91);
+            this.dataGridViewParent.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewParent.MultiSelect = false;
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.ReadOnly = true;
+            this.dataGridViewParent.RowHeadersWidth = 51;
+            this.dataGridViewParent.RowTemplate.Height = 29;
+            this.dataGridViewParent.Size = new System.Drawing.Size(350, 306);
+            this.dataGridViewParent.TabIndex = 5;
+            this.dataGridViewParent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewParent_CellClick);
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.AllowUserToAddRows = false;
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(383, 91);
+            this.dataGridViewChild.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.ReadOnly = true;
+            this.dataGridViewChild.RowHeadersWidth = 51;
+            this.dataGridViewChild.RowTemplate.Height = 29;
+            this.dataGridViewChild.Size = new System.Drawing.Size(434, 306);
+            this.dataGridViewChild.TabIndex = 6;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonDelete.Location = new System.Drawing.Point(322, 51);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(104, 28);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonAdd.Location = new System.Drawing.Point(206, 51);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(104, 28);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(269, 20);
+            this.textBoxTitle.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(95, 20);
+            this.textBoxTitle.TabIndex = 9;
+            // 
+            // textBoxGenre
+            // 
+            this.textBoxGenre.Location = new System.Drawing.Point(376, 20);
+            this.textBoxGenre.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxGenre.Name = "textBoxGenre";
+            this.textBoxGenre.Size = new System.Drawing.Size(95, 20);
+            this.textBoxGenre.TabIndex = 10;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(269, 5);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(27, 13);
+            this.labelTitle.TabIndex = 13;
+            this.labelTitle.Text = "Title";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonUpdate.Location = new System.Drawing.Point(437, 51);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(104, 28);
+            this.buttonUpdate.TabIndex = 16;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(921, 454);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.labelGenre);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.textBoxGenre);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Controls.Add(this.buttonConnect);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button buttonConnect;
+        private DataGridView dataGridViewParent;
+        private DataGridView dataGridViewChild;
+        private Button buttonDelete;
+        private Button buttonAdd;
+        private TextBox textBoxTitle;
+        private TextBox textBoxGenre;
+        private Label labelTitle;
+        private Button buttonUpdate;
+        private Label labelGenre;
+    }
+}
---
+++ src/DBMS/DBMSApp/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBMSApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source = DESKTOP-00EAF0L;" +
+            " Initial Catalog = bookstore; Integrated Security = True");
+        SqlDataAdapter da = new SqlDataAdapter();
+        BindingSource bsP = new BindingSource();
+        BindingSource bsC = new BindingSource();
+        DataSet dsP = new DataSet();
+        DataSet dsC = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM Authors", cs);
+            dsP.Clear();
+            da.Fill(dsP);
+            dataGridViewParent.DataSource = dsP.Tables[0];
+            bsP.DataSource = dsP.Tables[0];
+            bsP.MoveLast();
+            
+        }
+
+        private void dataGridViewParent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewParent.Rows[e.RowIndex].Cells[e.ColumnIndex].Value == null)
+                return;
+
+
+            string Id_Author = dataGridViewParent.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+
+            da.SelectCommand = new SqlCommand("SELECT * from Books " +
+                    "where AuthorID = " + Id_Author + "; ", cs);
+            dsC.Clear();
+            da.Fill(dsC);
+            dataGridViewChild.DataSource = dsC.Tables[0];
+            bsC.DataSource = dsC.Tables[0];
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewParent.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("Parent line must be selected!");
+                return;
+            }
+            else if(dataGridViewParent.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("Only one parent line must be selected!");
+                return;
+            }
+
+
+            da.InsertCommand = new
+                SqlCommand("INSERT INTO Books(Title,AuthorId,Genre)" +
+                    " VALUES (@T,@id,@G);", cs);
+            da.InsertCommand.Parameters.Add("@id",
+                SqlDbType.Int).Value = dsP.Tables[dataGridViewParent.CurrentCell.ColumnIndex].Rows[dataGridViewParent.CurrentCell.RowIndex][0];
+
+            da.InsertCommand.Parameters.Add("@T",
+                SqlDbType.VarChar).Value = textBoxTitle.Text;
+
+            da.InsertCommand.Parameters.Add("@G",
+                SqlDbType.VarChar).Value = textBoxGenre.Text;
+
+            cs.Open();
+            da.InsertCommand.ExecuteNonQuery();
+            cs.Close();
+            dsC.Clear();
+            da.Fill(dsC);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("A child line must be selected!");
+                return;
+            }
+            else if (dataGridViewChild.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("Only one child line must be selected!");
+                return;
+            }
+            da.DeleteCommand = new SqlCommand("Delete " +
+            "from Books where BookID = @id;", cs);
+
+            da.DeleteCommand.Parameters.Add("@id",
+                SqlDbType.Int).Value = dsC.Tables[0].Rows[dataGridViewChild.CurrentCell.RowIndex][0];
+
+            cs.Open();
+            da.DeleteCommand.ExecuteNonQuery();
+            cs.Close();
+            dsC.Clear();
+            da.Fill(dsC);
+        }
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewChild.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("A child line must be selected!");
+                return;
+            }
+            else if (dataGridViewChild.SelectedCells.Count > 1)
+            {
+                MessageBox.Show("Only one child line must be selected!");
+                return;
+            }
+
+            int x;
+            da.UpdateCommand = new SqlCommand("Update " +
+                "Books set Title = @T, Genre = @G" +
+                " where BookID=@id", cs);
+
+            da.UpdateCommand.Parameters.Add("@id",
+                SqlDbType.Int).Value = dsC.Tables[0].Rows[dataGridViewChild.CurrentCell.RowIndex][0];
+
+            da.UpdateCommand.Parameters.Add("@T",
+                SqlDbType.VarChar).Value = textBoxTitle.Text;
+
+            da.UpdateCommand.Parameters.Add("@G",
+                SqlDbType.VarChar).Value = textBoxGenre.Text;
+
+
+            cs.Open();
+            x = da.UpdateCommand.ExecuteNonQuery();
+            cs.Close();
+            dsC.Clear();
+            da.Fill(dsC);
+
+            if (x >= 1)
+                MessageBox.Show("The record has been updated");
+        }
+    }
+}
